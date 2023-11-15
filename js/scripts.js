71e190b19a5f4e5a8103f8fc8f192099df127d5a
alexa-estilo <150925803+alexa-estilo@users.noreply.github.com>
2023-11-15 14:54:51 +0100

    Add files via upload

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,13 @@
+WebFont.load({
+    google: {
+        families: ["Cormorant Garamond:300,300italic,regular,italic,500,500italic,600", "Roboto:300,300italic,regular,italic,500,500italic,700,700italic,900"]
+    }
+});
 
+
+!function (o, c) {
+    var n = c.documentElement
+        , t = " w-mod-";
+    n.className += t + "js",
+        ("ontouchstart" in o || o.DocumentTouch && c instanceof DocumentTouch) && (n.className += t + "touch")
+}(window, document);
